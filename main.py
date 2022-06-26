dff83ccd563fb463986796ddb0bf5409e9358134
RoksolanaMazurak <93178658+RoksolanaMazurak@users.noreply.github.com>
2022-06-26 14:10:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-from dbList import Dblist
-from employee import Employee
+import os
+
+from manager import Manager
 
 
-if __name__ == "__main__":
-    db_list = Dblist()
-    employee = Employee("Andrii", "Mykolenko", "none", "none", 13, 1995)
-    employee1 = Employee("Natalia", "Melnyk", "none", "none", 8, 1995)
-    employee2 = Employee("Roksolana", "Mazurak", "Lviv National Polytechnic University ",  "none", 0, 2004)
-    employee3 = Employee("Oleg", "Mazurak", "Lviv National University", "none", 5, 1995)
+def main():
 
-    db_list.add(employee)
-    db_list.add(employee1)
-    db_list.add(employee2)
-    db_list.add(employee3)
-    db_list.print_by_surname("Mazurak")
-    print()
+    file = "con267.tweetie.799608879"
+    file_location = os.path.abspath(file)
 
-    db_list.incertion_sort()
-
-    for e in db_list:
-        print(e)
+    Manager.gif_print(location=file_location)
 
 
+if __name__ == '__main__':
+    main()
 
